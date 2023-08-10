66010d1206e1c20176c500e4b94279dec63ac778
Akshat Bisht <109125241+Kaisama@users.noreply.github.com>
2023-08-10 19:51:40 +0530

    LeetCodeQues19RemoveNthNodeFromEndOfList.java

---
+++ linkedlist/assignment2/LeetCodeQues19RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,27 @@
+Class Solution{
+public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode start = new ListNode();
+        start.next = head;
+        ListNode fast = start;
+        ListNode slow = start;     
+
+        for(int i = 1; i <= n; ++i)
+            fast = fast.next;
+    
+        while(fast.next != null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        
+        slow.next = slow.next.next;
+        
+        return start.next;
+    }
+}
