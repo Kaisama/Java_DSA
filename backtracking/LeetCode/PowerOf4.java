718e1d04eeae24b19930b8caee331d2f0173df9d
Akshat Bisht <109125241+Kaisama@users.noreply.github.com>
2023-08-10 20:26:00 +0530

    PowerOf4.java

---
+++ backtracking/LeetCode/PowerOf4.java
@@ -0,0 +1,14 @@
+public class Solution2 {
+	public boolean isPowerOfFour(int n) {
+		if(n == 1)
+			return true;
+	        
+	    if(n == 0)
+	    	return false;
+	        
+	    if(n%4 != 0)
+	    	return false;
+	        
+	    return isPowerOfFour(n/4);
+	}      
+}
