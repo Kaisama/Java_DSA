61f3275c2859f42b2168de45db5924cbafe7c0a0
Akshat Bisht <109125241+Kaisama@users.noreply.github.com>
2023-08-10 19:40:12 +0530

    LeetCodeQues110BalancedBinaryTree.java

---
+++ trees/assignment2/LeetCodeQues110BalancedBinaryTree.java
@@ -0,0 +1,17 @@
+class Solution {
+    boolean ans = true;
+    public int solve(TreeNode root)
+    {
+        if(root == null)
+            return 0;
+        int left = solve(root.left);
+        int right = solve(root.right);
+        if(Math.abs(left-right)>1)
+            ans = false;
+        return Math.max(left,right)+1;
+    }
+    public boolean isBalanced(TreeNode root) {
+        solve(root);
+        return ans;
+    }
+}
